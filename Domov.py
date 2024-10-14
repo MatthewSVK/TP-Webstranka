914d4505db4fcb57ca24b3bc62ea46f1966080e9
Matúš Ďurovič <matus.durovic11@gmail.com>
2024-10-14 17:05:58 +0200

    zapisnica 2

--- Domov.py
+++ Domov.py
@@ -6,6 +6,7 @@ with open('zapisnice/zapisnica1.txt', 'r') as file:
     zapis1 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis1, file_name='zapisnica1.txt', mime='text/plain')
 
+st.subheader('Zápisnica 2')
 with open('zapisnice/zapisnica2.txt', 'r') as file:
     zapis1 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis1, file_name='zapisnica2.txt', mime='text/plain')
