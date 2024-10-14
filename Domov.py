877423d8760abfbe09b0c6481e0fa21c259648cb
Matúš Ďurovič <matus.durovic11@gmail.com>
2024-10-14 17:03:02 +0200

    zapisnica 2

--- Domov.py
+++ Domov.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-st.header('Zápisnice zo stretznutí')
+st.header('Zápisnice zo stretnutí')
 st.subheader('Zápisnica 1')
 with open('zapisnice/zapisnica1.txt', 'r') as file:
     zapis1 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis1, file_name='zapisnica1.txt', mime='text/plain')
+
+with open('zapisnice/zapisnica2.txt', 'r') as file:
+    zapis1 = file.read()
+st.download_button('Stiahnuť zápisnicu', data=zapis1, file_name='zapisnica2.txt', mime='text/plain')
