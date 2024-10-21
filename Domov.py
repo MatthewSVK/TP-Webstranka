31e8e285cbdfc12ac0d69c71934e07e1f3ae4236
MartinDzurko111218 <xdzurko@stuba.sk>
2024-10-21 16:37:56 +0200

    uprava permissions na rb

--- Domov.py
+++ Domov.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 st.header('Zápisnice zo stretnutí')
 st.subheader('Zápisnica 1')
-with open('zapisnice/TP_Zapisnica1.pdf', 'r') as file:
+with open('zapisnice/TP_Zapisnica1.pdf', 'rb') as file:
     zapis1 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis1, file_name='TP_Zapisnica1.pdf', mime='application/octet-stream')
 
 st.subheader('Zápisnica 2')
-with open('zapisnice/TP_Zapisnica2.pdf', 'r') as file:
+with open('zapisnice/TP_Zapisnica2.pdf', 'rb') as file:
     zapis2 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis2, file_name='TP_Zapisnica2.pdf', mime='application/octet-stream')
 
 st.subheader('Zápisnica 3')
-with open('zapisnice/TP_Zapisnica3.pdf', 'r') as file:
+with open('zapisnice/TP_Zapisnica3.pdf', 'rb') as file:
     zapis3 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis3, file_name='TP_Zapisnica3.pdf', mime='application/octet-stream')
