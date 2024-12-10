f9304ae04cc5ec5a07962284eafa2525cd25a7bb
MartinDzurko111218 <xdzurko@stuba.sk>
2024-12-10 10:55:30 +0100

    Doplnenie zapisnic zo stretnuti

--- Domov.py
+++ Domov.py
@@ -25,3 +25,13 @@ st.subheader('Zápisnica 5')
 with open('zapisnice/TP_Zapisnica5.pdf', 'rb') as file:
     zapis5 = file.read()
 st.download_button('Stiahnuť zápisnicu', data=zapis5, file_name='TP_Zapisnica5.pdf', mime='application/octet-stream')
+
+st.subheader('Zápisnica 6')
+with open('zapisnice/TP_Zapisnica6.pdf', 'rb') as file:
+    zapis6 = file.read()
+st.download_button('Stiahnuť zápisnicu', data=zapis6, file_name='TP_Zapisnica6.pdf', mime='application/octet-stream')
+
+st.subheader('Zápisnica 7')
+with open('zapisnice/TP_Zapisnica7.pdf', 'rb') as file:
+    zapis7 = file.read()
+st.download_button('Stiahnuť zápisnicu', data=zapis7, file_name='TP_Zapisnica7.pdf', mime='application/octet-stream')
